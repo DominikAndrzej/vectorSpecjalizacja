9ddec101e686cbc8d5bfbe8281094a3eef6ce189
unknown <dominikssj2@dom.sci.edu.pl>
2022-03-22 15:38:36 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/vectorSpecjalizacja.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectorSpecjalizacja.dir/main.cpp.obj"
+  "vectorSpecjalizacja.exe"
+  "vectorSpecjalizacja.exe.manifest"
+  "vectorSpecjalizacja.lib"
+  "vectorSpecjalizacja.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vectorSpecjalizacja.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
